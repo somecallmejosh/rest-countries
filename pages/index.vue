10c8446113a12017f5bc9cf781dd46afbae60d02
Joshua Briley <josh@thebrileys.com>
2019-11-24 06:00:46 -0500

    Accessibility update.

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,13 @@
   <div>
     <div class="filters">
       <div class="form-field-wrap">
+        <label for="search" class="visuallyhidden">Search</label>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"/></svg>
-        <input type="search" v-model="search" placeholder="Search for a country" />
+        <input type="search" id="search" name="search" v-model="search" placeholder="Search for a country" />
       </div>
 
       <div class="form-field-wrap">
+        <label for="regions" class="visuallyhidden">Filter by Region</label>
         <select name="regions" id="regions" v-model="search">
         <option value class="placeholder" hidden>Filter by region</option>
         <template v-for="item in filteredRegions">
@@ -225,6 +227,16 @@ select::-ms-expand {
   /* Firefox 18- */
   color: var(--text-white);
   opacity: 1;
+}
+
+.visuallyhidden {
+  position: absolute !important;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
+  clip: rect(1px, 1px, 1px, 1px);
+  white-space: nowrap; /* added line */
 }
 
 @media (min-width: 1024px) {
